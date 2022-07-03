52c1f65690cf8ba11a7bf46a5ae850c45028e9d2
konnodonnoi <saboanamero8521@gmail.com>
2022-07-02 20:07:02 -1200

    commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@
 import './App.css';
 
 function App() {
+  const billsArray = ["house bills","netflix bills","mama bills","feeding bills","",""]
+  const billsElement = billsArray.map(bill => <p key={bill}>{bill}</p>)
+
+  function addItem() {
+    const newItem = `thing ${billsArray.length + 1}`
+    billsArray.push(newItem)
+console.log(billsArray)
+  }
+  
   return (
-    <div className="App">
-   hi
+    <div className="">
+   <button className='button' onClick={addItem}>add item</button>
+   {billsElement}
 
     </div>
   );
